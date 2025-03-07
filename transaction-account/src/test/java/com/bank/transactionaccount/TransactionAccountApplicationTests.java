1edb7b94c24d16e2a5cfcc261649bfa0386a7346
Joel-x-x <acostajoel0001@gmail.com>
2025-03-06 20:18:35 -0500

    feat: add project

---
+++ transaction-account/src/test/java/com/bank/transactionaccount/TransactionAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.transactionaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
